ad8af77582aaa0bc19418ec00f3f2b737f2cb1c6
LiteGAMMA <fanmasterprofanmasterpro@gmail.com>
2026-02-17 19:34:58 +0300

    Update LiteGamma Tools Full Version.py

--- LiteGamma Tools Full Version.py
+++ LiteGamma Tools Full Version.py
@@ -34,7 +34,7 @@ GITHUB_RAW_BASE = f"https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO
 GITHUB_API_BASE = f"https://api.github.com/repos/{GITHUB_USER}/{GITHUB_REPO}"
 
 # =============== ВЕРСИЯ ПРОГРАММЫ ===============
-CURRENT_VERSION = "1.2.3"  
+CURRENT_VERSION = "1.2.2"  
 UPDATE_CHECK_INTERVAL = 3600
 LAST_UPDATE_CHECK_FILE = "last_update_check.json"
 AUTO_UPDATE = True
@@ -2409,4 +2409,5 @@ if __name__ == '__main__':
     except Exception as e:
         print(f"\n{Fore.RED}✘ Ошибка: {e}{Style.RESET_ALL}")
         traceback.print_exc()
+
 
